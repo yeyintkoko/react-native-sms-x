0a6c8182f4b1228983a7e4dd5362e1c38f9965a2
yeyintkoko <yeyintkoko15@gmail.com>
2016-11-08 17:32:24 +0630

    fix bug No.9584

--- android/app/src/main/java/com/someone/sendsms/SendSMS.java
+++ android/app/src/main/java/com/someone/sendsms/SendSMS.java
@@ -19,6 +19,7 @@ import com.facebook.react.bridge.ReactMethod;
 public class SendSMS extends ReactContextBaseJavaModule {
 
     private final ReactApplicationContext reactContext;
+    private Callback callback = null;
 
     public SendSMS(ReactApplicationContext reactContext) {
 
@@ -31,64 +32,82 @@ public class SendSMS extends ReactContextBaseJavaModule {
         return "SendSMS";
     }
 
+    private void sendCallback(String message){
+        if (callback != null) {
+            callback.invoke(message);
+            callback = null;
+        }
+    }
+
     //---sends an SMS message to another device---
     @ReactMethod
-    public void send(String phoneNumber, String message, final Callback callback ){
+    public void send(String phoneNumber, String message, final Callback cb ){
 
-        String SENT = "SMS_SENT";
-        String DELIVERED = "SMS_DELIVERED";
+        try{
 
-        PendingIntent sentPI = PendingIntent.getBroadcast(reactContext, 0,
-                new Intent(SENT), 0);
+            this.callback = cb;
 
-        PendingIntent deliveredPI = PendingIntent.getBroadcast(reactContext, 0,
-                new Intent(DELIVERED), 0);
+            String SENT = "SMS_SENT";
+            String DELIVERED = "SMS_DELIVERED";
 
-        //---when the SMS has been sent---
-        reactContext.registerReceiver(new BroadcastReceiver(){
-            @Override
-            public void onReceive(Context arg0, Intent arg1) {
-                switch (getResultCode())
-                {
-                    case Activity.RESULT_OK:
-                        callback.invoke("SMS sent");
-                        break;
-                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
-                        callback.invoke("Generic failure");
-                        break;
-                    case SmsManager.RESULT_ERROR_NO_SERVICE:
-                        callback.invoke("No service");
-                        break;
-                    case SmsManager.RESULT_ERROR_NULL_PDU:
-                        callback.invoke("Null PDU");
-                        break;
-                    case SmsManager.RESULT_ERROR_RADIO_OFF:
-                        callback.invoke("Radio off");
-                        break;
+            PendingIntent sentPI = PendingIntent.getBroadcast(reactContext, 0,
+                    new Intent(SENT), 0);
+
+            PendingIntent deliveredPI = PendingIntent.getBroadcast(reactContext, 0,
+                    new Intent(DELIVERED), 0);
+
+            //---when the SMS has been sent---
+            reactContext.registerReceiver(new BroadcastReceiver(){
+                @Override
+                public void onReceive(Context arg0, Intent arg1) {
+                    switch (getResultCode())
+                    {
+                        case Activity.RESULT_OK:
+                            sendCallback("SMS sent");
+                            break;
+                        case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                            sendCallback("Generic failure");
+                            break;
+                        case SmsManager.RESULT_ERROR_NO_SERVICE:
+                            sendCallback("No service");
+                            break;
+                        case SmsManager.RESULT_ERROR_NULL_PDU:
+                            sendCallback("Null PDU");
+                            break;
+                        case SmsManager.RESULT_ERROR_RADIO_OFF:
+                            sendCallback("Radio off");
+                            break;
+                    }
                 }
-            }
-        }, new IntentFilter(SENT));
+            }, new IntentFilter(SENT));
 
-        //---when the SMS has been delivered---
-        reactContext.registerReceiver(new BroadcastReceiver(){
-            @Override
-            public void onReceive(Context arg0, Intent arg1) {
-                switch (getResultCode())
-                {
-                    case Activity.RESULT_OK:
-                        System.out.println("SMS delivered");
-                        //callback.invoke("SMS delivered");
-                        break;
-                    case Activity.RESULT_CANCELED:
-                        System.out.println("SMS not delivered");
-                        //callback.invoke("SMS not delivered");
-                        break;
+            //---when the SMS has been delivered---
+            reactContext.registerReceiver(new BroadcastReceiver(){
+                @Override
+                public void onReceive(Context arg0, Intent arg1) {
+                    switch (getResultCode())
+                    {
+                        case Activity.RESULT_OK:
+                            System.out.println("SMS delivered");
+                            sendCallback("SMS delivered");
+                            break;
+                        case Activity.RESULT_CANCELED:
+                            System.out.println("SMS not delivered");
+                            sendCallback("SMS not delivered");
+                            break;
+                    }
                 }
-            }
-        }, new IntentFilter(DELIVERED));
+            }, new IntentFilter(DELIVERED));
 
-        SmsManager sms = SmsManager.getDefault();
-        sms.sendTextMessage(phoneNumber, null, message, sentPI, deliveredPI);
+            SmsManager sms = SmsManager.getDefault();
+            sms.sendTextMessage(phoneNumber, null, message, sentPI, deliveredPI);
+
+        }catch (Exception e) {
+
+            sendCallback("Unknown error");
+            throw e;
+
+        }
 
     }
 
